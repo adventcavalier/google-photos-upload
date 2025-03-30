da37cfea721d59c712e6254c251febece52445f1
Jonathan Hart <jonathan.e.hart@gmail.com>
2025-03-29 21:12:08 -0400

    Add Python packaging configuration

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="gphotos-upload",
+    version="1.0.0",
+    author="Advent Cavalier",
+    author_email="jonathan.e.hart@gmail.com",
+    description="A tool for uploading photos to Google Photos with automatic album organization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adventcavalier/gphotos-upload",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.7",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "gphotos-upload=gphotos_upload.__main__:main",
+        ],
+    },
+) 
